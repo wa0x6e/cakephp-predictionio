7506c515a8083f1cbd7914ef1dc8427121276ed8
Kamisama <kami@kamisama.me>
2013-11-09 22:05:02 -0500

    Fix coding standard

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -7,14 +7,14 @@
 	));
 
 	$files = array(
-	    App::pluginPath('PredictionIO') . 'vendor' . DS . 'autoload.php',
-	     APP . 'Vendor' . DS . 'autoload.php',
-	     APP . 'vendor' . DS . 'autoload.php',
+		App::pluginPath('PredictionIO') . 'vendor' . DS . 'autoload.php',
+		 APP . 'Vendor' . DS . 'autoload.php',
+		 APP . 'vendor' . DS . 'autoload.php',
 	);
 
 	foreach ($files as $file) {
-	    if (file_exists($file)) {
-	        require_once $file;
-	        break;
-	    }
+		if (file_exists($file)) {
+			require_once $file;
+			break;
+		}
 	}
